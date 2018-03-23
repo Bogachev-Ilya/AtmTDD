026e51937ac253150300fd6e6465bd2f6541f8f5
Ilya Bogachev <32776363+Bogachev-Ilya@users.noreply.github.com>
2018-03-23 19:36:23 +0300

    Update AtmTest.java

--- test/AtmTest.java
+++ test/AtmTest.java
@@ -1,10 +1,8 @@
 import org.junit.Test;
-
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
-
 import static org.junit.Assert.*;
 
 public class AtmTest {
